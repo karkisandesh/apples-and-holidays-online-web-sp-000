31e2d74c39f53c0f09135be09812c3787904f545
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-30 04:43:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,8 +42,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  all_items = [ ]
-  holiday_hash.each do |holiday, supply_hash|
+ holiday_hash.each do |holiday, supply_hash|
       if holiday == :winter
         supply_hash.each do |events, items|
         items
@@ -51,8 +50,6 @@ def all_winter_holiday_supplies(holiday_hash)
        end
     end
   end
-  all_items
-  
 end
 
 def all_supplies_in_holidays(holiday_hash)
