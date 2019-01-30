dd4b106401744c751a3921c05ed2502ecf775c05
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-30 05:22:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
            array.each do |word|
              word.capitalize!
            end
-                events = word.join
+              events = word.join
                     puts  "#{events} : #{supplies}"
         end
       end
