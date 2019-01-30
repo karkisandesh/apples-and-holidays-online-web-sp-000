6c264eb67b45c700b67058db42bfc26fcf8bef74
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-30 05:01:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, supply_hash|
     "#{season} : {supply_hash_hash.each do |events, supplies|
-                #{events}.to_s: #{supplies}""
+                #{events}.to_s: #{supplies}"
       
                 end
   end
