7b8f5ce88e91b5eb121874af929c1801ee72a628
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-30 05:31:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
                     supply_hash.each do |events, supplies|
                         array = events.to_s.split(" ")
                            array.each do |word|
-                                  word.capitalize!
+                                  word = word.capitalize!
                                   event = word.join
                                   binding.pry
                                   puts  "#{event} : #{supplies}"
