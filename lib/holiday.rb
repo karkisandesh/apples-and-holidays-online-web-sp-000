b6d670b31684f78b81ee1e44d01cbba8bb3bd5b1
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-30 05:55:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,7 +79,7 @@ def all_holidays_with_bbq(holiday_hash)
   array = [ ]
     holiday_hash.collect do |season, supply_hash|
         supply_hash.collect do |events, items|
-                if  items.include?("BBQ")
+                if  items == :BBQ
                   events
             end    
         end
