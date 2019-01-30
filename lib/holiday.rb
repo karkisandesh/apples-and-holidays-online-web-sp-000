7ebc13ad1b275dfc25c919bc1bd9f9a54af0b048
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-30 04:21:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,7 +39,6 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_has[:season][:holiday_name] = supply_array
   holiday_hash
-
 end
 
 def all_winter_holiday_supplies(holiday_hash)
